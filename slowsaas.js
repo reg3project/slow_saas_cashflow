702e6783d4f4ff28e25563c556ce5e9c6bc29291
DESKTOP-KD5J6EQ\Luca1 <luca.reginato@pagination.com>
2019-03-08 17:06:34 +0100

    first real code

--- slowsaas.js
+++ slowsaas.js
@@ -1,4 +1,21 @@
-console.log("hello world")
+/* console.log("hello world") */
 const fsLib = require("fs")
-const fileContent = fs.readFileSync("./Data/Recurring_Expense_test.csv")
-console.log(fileContent.toString());
+var fileContent = fsLib.readFileSync("./Data/Recurring_Expense_test.csv")
+fileContent = fileContent.toString()
+// Divid the file into Rows
+var array1 = fileContent.split("\r\n");
+var final_arr = [];
+for (var i = 1; i < array1.length; i++) {
+    let cols = array1[i].split(",");
+    final_arr.push(cols);
+    if(cols[14] && cols[20]){
+        let total = cols[14] * cols[20];
+        let date = new Date(cols[3]);
+        if(cols[1] == "Months"){
+            date.setUTCMonth(date.getUTCMonth()+ parseInt(cols[2]));
+        }else if(cols[1]=="Years"){
+            date.setFullYear(date.getFullYear()+ parseInt(cols[2]));
+        }
+    } 
+} 
+
