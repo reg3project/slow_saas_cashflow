75eaa63329b7528ed83e56b9ce12e63eda4a16fa
AhmedMahmoud2012 <ahmedcs2012@gmail.com>
2019-03-09 22:30:35 +0200

    run `npm install` before run the script

--- slowsaas.js
+++ slowsaas.js
@@ -1,21 +1,56 @@
-/* console.log("hello world") */
-const fsLib = require("fs")
-var fileContent = fsLib.readFileSync("./Data/Recurring_Expense_test.csv")
-fileContent = fileContent.toString()
-// Divid the file into Rows
-var array1 = fileContent.split("\r\n");
-var final_arr = [];
-for (var i = 1; i < array1.length; i++) {
-    let cols = array1[i].split(",");
-    final_arr.push(cols);
-    if(cols[14] && cols[20]){
-        let total = cols[14] * cols[20];
-        let date = new Date(cols[3]);
-        if(cols[1] == "Months"){
-            date.setUTCMonth(date.getUTCMonth()+ parseInt(cols[2]));
-        }else if(cols[1]=="Years"){
-            date.setFullYear(date.getFullYear()+ parseInt(cols[2]));
-        }
-    } 
-} 
+const moment = require("moment");
 
+const fsLib = require("fs");
+// external lib so I can generate output excel file..
+const excelLib = require("excel4node");
+// Create Work Book...
+const workbook = new excelLib.Workbook();
+// Create Work Sheet..
+var workSheet = workbook.addWorksheet("Sheet 1");
+
+const repeatedStyle = workbook.createStyle({
+  font: {
+    color: "#c0392b",
+    size: 12
+  }
+});
+const normalStyle = workbook.createStyle({
+  font: {
+    color: "#000000",
+    size: 12
+  }
+});
+var fileContent = fsLib.readFileSync("./Data/Recurring_Expense_test.csv");
+fileContent = fileContent.toString();
+// Divid the file into Rows
+var array1 = fileContent.split("\n");
+for (var i = 1, k = 1, l = 2; i < array1.length; i++, k += 2, l += 2) {
+  let cols = array1[i].split(",");
+  // add row items into the output file..
+  cols.forEach((itemValue, j) => {
+    workSheet
+      .cell(k, j + 1)
+      .string(itemValue + "")
+      .style(normalStyle);
+  });
+
+  if (cols[14] && cols[20]) {
+    let amount = cols[14] * cols[20];
+    let date = new Date(cols[3]);
+    if (cols[1] == "Months") {
+      date.setUTCMonth(date.getUTCMonth() + parseInt(cols[2]));
+    } else if (cols[1] == "Years") {
+      date.setFullYear(date.getFullYear() + parseInt(cols[2]));
+    }
+    cols[3] = moment(date).format("YYYY-MM-DD");
+    cols[18] = amount;
+    cols.forEach((itemValue, j) => {
+      workSheet
+        .cell(l, j + 1)
+        .string(itemValue + "")
+        .style(repeatedStyle);
+    });
+  }
+}
+
+workbook.write("./Output/output.xlsx");
